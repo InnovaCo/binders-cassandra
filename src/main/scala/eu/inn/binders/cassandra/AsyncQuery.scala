5dcab550a1bbeaa735d1a8051c922ea9fbe0a06a
Maga Abdurakhmanov <maga@inn.eu>
2014-05-05 16:02:22 +0300

    removed global execution context

--- src/main/scala/eu/inn/binders/cassandra/AsyncQuery.scala
+++ src/main/scala/eu/inn/binders/cassandra/AsyncQuery.scala
@@ -1,28 +1,26 @@
 package eu.inn.binders.cassandra
 
 import com.datastax.driver.core.{ResultSet, BoundStatement, Session}
-import scala.concurrent.{ExecutionContext, Promise, Future}
+import scala.concurrent.{Promise, Future}
 import com.google.common.util.concurrent.{FutureCallback, Futures}
 
 
 class AsyncQuery(val session: Session, val queryString: String) extends eu.inn.binders.core.Query[Future[Rows], Statement]{
   val preparedStatement = session.prepare(queryString)
 
-	private class FutureConverter(promise: Promise[ResultSet]) extends FutureCallback[ResultSet] {
+	private class FutureConverter(promise: Promise[Rows]) extends FutureCallback[ResultSet] {
 		override def onFailure(t: Throwable): Unit = promise.failure(t)
-		override def onSuccess(result: ResultSet): Unit = promise.success(result)
+		override def onSuccess(result: ResultSet): Unit = promise.success(new Rows(result))
 	}
 
   def bindAndExecute(f: (Statement) => Unit): Future[Rows] = {
-		import ExecutionContext.Implicits.global
-
     val boundStatement = new BoundStatement(preparedStatement)
     val statement = new Statement(boundStatement)
     f(statement)
 		val futureResult = session.executeAsync(boundStatement)
-		val promise = Promise[ResultSet]
+		val promise = Promise[Rows]
 		val conv = new FutureConverter(promise)
 		Futures.addCallback(futureResult, conv)
-		promise.future map(new Rows(_))
+		promise.future
   }
 }
